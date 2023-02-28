e9fc2aebba03e461e58b61f5316367f0a8e8ae5a
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-02-28 08:39:19 +0530

    simple commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello fucking world!.");
+    res.send("Hello sheet world!.");
 });
 
 export default app;
