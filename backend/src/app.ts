48331a24776125eb598275f54d70e5a4e49c40d9
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-03-01 17:09:19 +0530

    Express http error handling.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
 import morgan from "morgan";
+import createHttpError, {isHttpError } from "http-errors";
 
 const app = express();
 
@@ -12,14 +13,20 @@ app.use(express.json());
 app.use("/api/notes/",notesRoutes);
 
 app.use((req,res,next) => {
-    next(Error("Endpoint not found."));
+    next(createHttpError(404, "Endpoint not found."));
 })
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction)=>{
     console.log(error);
     let errorMessage = "An unknown error occured.";
+    let statusCode = 500;
+    if(isHttpError(error)){
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
     if(error instanceof Error) errorMessage = error.message;
-    res.status(500).json({error:errorMessage});
+    res.status(statusCode).json({error:errorMessage});
 });
 
 export default app;
+
--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,3 +1,5 @@
+import mongoose  from 'mongoose';
+import createHttpError from 'http-errors';
 import { RequestHandler } from "express";
 import NoteModel from '../models/note';
 
@@ -13,17 +15,30 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 export const getNote: RequestHandler = async (req, res, next) => {
     const noteId = req.params.noteId;
     try{
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400,"Invalid note id.")
+        }
         const note = await NoteModel.findById(noteId).exec();
+
+        if(!note){
+            throw createHttpError(404, "Note not found.");
+        }
         res.status(200).json(note);
     }catch(error){
         next(error);
     }
 }
-
-export const createNotes: RequestHandler = async (req, res, next) => {
+interface CreateNoteBody {
+    title?: string,
+    text?: string
+}
+export const createNotes: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
     try{
+        if(!title){
+            throw createHttpError(400,"Note must have a title.");
+        }
         const newNote = await NoteModel.create({
             title:title,
             text:text
