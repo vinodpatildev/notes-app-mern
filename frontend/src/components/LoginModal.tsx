b90b2940cfd835994a8d01c86f8bbcea83084b56
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-03-05 09:04:14 +0530

    handling different http errors in react.

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -2,9 +2,11 @@ import { useForm } from "react-hook-form";
 import { User } from "../models/user";
 import { LoginCredentials } from "../network/notes_api";
 import * as NotesApi from "../network/notes_api";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Alert, Button, Form, Modal } from "react-bootstrap";
 import  TextInputField  from "./form/TextInputField";
 import styleUtils from "../styles/utils.module.css";
+import {useState} from "react";
+import { UnauthorizedError } from "../errors/http_errors";
 
 interface LoginModalProps{
     onDismiss: () => void,
@@ -12,6 +14,9 @@ interface LoginModalProps{
 }
 
 const LoginModal = ({onDismiss, onLoginSuccessful}: LoginModalProps) => {
+
+    const [errorText, setErrorText] = useState<string|null>(null);
+
     const { register, handleSubmit, formState: {errors, isSubmitting} } = useForm<LoginCredentials>();
 
     async function onSubmit(credentials: LoginCredentials) {
@@ -19,7 +24,11 @@ const LoginModal = ({onDismiss, onLoginSuccessful}: LoginModalProps) => {
             const newUser = await NotesApi.login(credentials);
             onLoginSuccessful(newUser);
         } catch(error){
-            alert(error);
+            if(error instanceof UnauthorizedError){
+                setErrorText(error.message);
+            }else{
+                alert(error);
+            }
             console.error(error);
         }
     }
@@ -32,6 +41,11 @@ const LoginModal = ({onDismiss, onLoginSuccessful}: LoginModalProps) => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    {errorText &&
+                        <Alert variant="danger">
+                            {errorText}
+                        </Alert>
+                    }
                     <Form onSubmit={handleSubmit(onSubmit)}>
                         <TextInputField
                             name="username"
--- frontend/src/components/SignUpModal.tsx
+++ frontend/src/components/SignUpModal.tsx
@@ -2,9 +2,11 @@ import { useForm } from "react-hook-form";
 import { User } from "../models/user";
 import { SignUpCredentials } from "../network/notes_api";
 import * as NotesApi from "../network/notes_api";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Alert, Button, Form, Modal } from "react-bootstrap";
 import  TextInputField  from "./form/TextInputField";
 import styleUtils from "../styles/utils.module.css";
+import { ConflictError } from "../errors/http_errors";
+import { useState } from "react";
 
 interface SignUpModalProps{
     onDismiss: () => void,
@@ -12,6 +14,9 @@ interface SignUpModalProps{
 }
 
 const SignUpModal = ({onDismiss, onSignUpSuccessful}: SignUpModalProps) => {
+
+    const [errorText, setErrorText] = useState<string|null>(null);
+
     const { register, handleSubmit, formState: {errors, isSubmitting} } = useForm<SignUpCredentials>();
 
     async function onSubmit(credentials: SignUpCredentials) {
@@ -19,7 +24,11 @@ const SignUpModal = ({onDismiss, onSignUpSuccessful}: SignUpModalProps) => {
             const newUser = await NotesApi.signUp(credentials);
             onSignUpSuccessful(newUser);
         } catch(error){
-            alert(error);
+            if(error instanceof ConflictError){
+                setErrorText(error.message);
+            }else{
+                alert(error);
+            }
             console.error(error);
         }
     }
@@ -32,6 +41,11 @@ const SignUpModal = ({onDismiss, onSignUpSuccessful}: SignUpModalProps) => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    {errorText &&
+                        <Alert variant="danger">
+                            {errorText}
+                        </Alert>
+                    }
                     <Form onSubmit={handleSubmit(onSubmit)}>
                         <TextInputField
                             name="username"
